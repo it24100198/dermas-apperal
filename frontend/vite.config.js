b51df3477234499c45061bd366c864368e2b3ef9
pavinthan20 <it24100198@my.sliit.lk>
2026-04-08 22:30:00 +0530

    updated project files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const apiProxyTarget = env.VITE_DEV_API_PROXY_TARGET || 'http://localhost:5001';
-
-  return {
-    plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:5001',
-          changeOrigin: true
-        }
-      }
-    }
-  };
-});
-=======
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
->>>>>>> 4fdb989 (first commit)
